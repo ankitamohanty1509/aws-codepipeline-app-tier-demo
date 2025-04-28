2c89746b53a6e7273b3481ab2eea68f94ee4c27b
jatharthan <jatharthan94@gmail.com>
2025-04-28 16:52:46 -0400

    folder structure

--- app-tier/DbConfig.js
+++ app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'bootcamp1-aurora-cluster.cluster-cw94i4e2ijk2.us-east-1.rds.amazonaws.com',
+    DB_HOST : 'project9-aurora-writer.cw94i4e2ijk2.us-east-1.rds.amazonaws.com',
     DB_USER : 'jatharthan',
     DB_PWD : 'jatharthan',
     DB_DATABASE : 'webappdb'
--- scripts/start_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/app-tier
-pm2 start index.js
-pm2 save
