45e4044cd074e72305cb536739ceae6ae28b7335
jatharthan <jatharthan94@gmail.com>
2025-04-28 23:15:12 -0400

    updates

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Set NVM_DIR explicitly to /home/ec2-user/.nvm and source nvm.sh
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 cd ~/app-tier
 npm install
