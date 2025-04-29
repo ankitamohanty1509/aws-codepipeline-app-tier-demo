bd3e4eb0116ecb4ad7d31ee0ba4db5042a11b699
jatharthan <jatharthan94@gmail.com>
2025-04-28 23:25:01 -0400

    updates

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Set NVM_DIR explicitly to /home/ec2-user/.nvm and source nvm.sh
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 cd ~/app-tier
 pm2 start index.js
 pm2 save
-pm2 startup systemd -u ec2-user --hp /home/ec2-user
