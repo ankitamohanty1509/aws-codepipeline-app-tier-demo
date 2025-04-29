80dd5a85bc241d9d69c9baff8e12d48a4e451598
jatharthan <jatharthan94@gmail.com>
2025-04-28 23:38:58 -0400

    updates

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,7 @@
 export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app-tier
+
 cd ~/app-tier
 npm install
